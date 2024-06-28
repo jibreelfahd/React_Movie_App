31ee4dad22e5be56aba7cf2e5426cc142f365e81
Fahd Jibril <jibreelfahd@gmail.com>
2024-06-28 23:56:48 +0100

    Added add movie feature

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import React, { useState, useCallback, useEffect } from "react";
 
 import MovieList from "./components/Movies/MovieList";
 import Card from "./components/UI/Card/Card";
+import AddMovie from "./components/Movies/AddMovie";
 
 import styles from "./App.module.css";
+import Button from "./components/UI/Button/Button";
 
 const App = () => {
   const [movies, setMovies] = useState([]);
@@ -15,23 +17,23 @@ const App = () => {
     setError(null);
 
     try {
-    const response = await fetch("https://swapi.dev/api/films");
+      const response = await fetch("https://swapi.dev/api/films");
 
-    if (!response.ok) {
-      throw new Error('Something went wrong, Try Again Later!!');
-    }
-    const data = await response.json();
+      if (!response.ok) {
+        throw new Error("Something went wrong, Try Again Later!!");
+      }
+      const data = await response.json();
 
-    const transformedData = data.results.map((moviesData) => {
-      return {
-        id: moviesData.episode_id,
-        tittle: moviesData.title,
-        openingText: moviesData.opening_crawl,
-        releaseDate: moviesData.release_date,
-      };
-    });
+      const transformedData = data.results.map((moviesData) => {
+        return {
+          id: moviesData.episode_id,
+          tittle: moviesData.title,
+          openingText: moviesData.opening_crawl,
+          releaseDate: moviesData.release_date,
+        };
+      });
 
-    setMovies(transformedData);
+      setMovies(transformedData);
     } catch (error) {
       setError(error.message);
     }
@@ -42,14 +44,34 @@ const App = () => {
     fetchMoviesHandler();
   }, [fetchMoviesHandler]);
 
+  const addMovieHandler = async (movie) => {
+    const response = await fetch('https://swapi.dev/api/films', {
+      method: 'POST',
+      body: JSON.stringify(movie),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+
+    const data = await response.json();
+    console.log(data);
+  };
+
   return (
     <>
+      <section>
+        <AddMovie onAddMovie={addMovieHandler} />
+      </section>
       <Card className={styles["fetch--movies"]}>
-        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+        <Button onClick={fetchMoviesHandler}>Fetch Movies</Button>
       </Card>
       <section>
-        {!isLoading   && (
-          <MovieList movies={movies} isMovieLoading={isLoading} isError={error }/>
+        {!isLoading && (
+          <MovieList
+            movies={movies}
+            isMovieLoading={isLoading}
+            isError={error}
+          />
         )}
       </section>
     </>
